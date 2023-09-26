e40738fedd8bebb67847bc5420ad7895c1e3db07
yohanSebastian5 <yspacheco90@misena.edu.co>
2023-09-26 18:47:21 -0500

    Landing page started

---
+++ landing/index.js
@@ -0,0 +1,17 @@
+import * as THREE from"https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
+import * as DOTS from "https://cdn.jsdelivr.net/npm/vanta@0.5.24/dist/vanta.dots.min.js"
+
+VANTA.DOTS({
+    el: "#containerPuntos",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0xffffff,
+    size: 7.10,
+    spacing: 24.00,
+    showLines: false
+  })
